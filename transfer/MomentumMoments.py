d7f63ba34db95ed11ee300e76bce402f4475b8ce
Nick Hand <nhand@berkeley.edu>
2015-12-19 14:40:55 -0500

    new moments transfer that appear to be working

--- transfer/MomentumMoments.py
+++ transfer/MomentumMoments.py
@@ -1,26 +1,44 @@
 from nbodykit.extensionpoints import Transfer
 import numpy
 
-class MomentumMoments(Transfer):
+
+class MomentumMomentsAuto(Transfer):
     """
     Transfer function for radial momentum moments
     """
-    plugin_name = "MomentumMoments"
+    plugin_name = "MomentumMomentsAuto"
+
+    @classmethod
+    def register(kls):
+        h = kls.parser
+        h.add_argument("ell", type=int, help="the radial velocity moment")
+  
+    def __call__(self, pm, complex):
+        
+        kern = 1.0 / numpy.math.factorial(self.ell)    
+        complex[:] *= kern
+        
+class MomentumMomentsCross(Transfer):
+    """
+    Transfer function for radial momentum moments
+    """
+    plugin_name = "MomentumMomentsCross"
 
     @classmethod
     def register(kls):
         h = kls.parser
         h.add_argument("ell", type=int, help="the 1st radial velocity moment")
         h.add_argument("ell_prime", type=int, help="the 2nd radial velocity moment")
-
+  
     def __call__(self, pm, complex):
         
         norm = numpy.math.factorial(self.ell) * numpy.math.factorial(self.ell_prime)
         if self.ell == self.ell_prime:
             kern = 1.0 / norm
         else:
-            power = self.ell + self.ell_prime + 1
-            kern = (-1)**(power) * 2.0 / norm
+            sign = (-1)**self.ell_prime * numpy.sign((1j)**(self.ell+self.ell_prime)).real
+            kern = sign * (2. / norm)
+            
         complex[:] *= kern
         
         
